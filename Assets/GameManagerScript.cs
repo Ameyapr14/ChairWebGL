930e15a7c887dbe11cc8d6489745335cfc629128
Ameyapr14 <ampa@ciklum.com>
2025-06-09 14:45:59 +0530

    Button Setup and Scene Changes done

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    [SerializeField] public GameObject[] ScreenButtons;
+    [SerializeField] public CameraOrbit Camref;
+    [SerializeField] public RotateScript[] RotationS;
+    [SerializeField] public Camera Cam;
+
+}
