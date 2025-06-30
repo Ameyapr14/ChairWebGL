f893125ec45d384630d5a7390b819741438b777c
Ameyapr14 <ampa@ciklum.com>
2025-06-30 14:42:00 +0530

    Done with final functional UI work and link added

--- Assets/GameManagerAuction.cs
+++ Assets/GameManagerAuction.cs
@@ -43,75 +43,78 @@ public class GameManagerAuction : MonoBehaviour
         Square.transform.localScale = new Vector3(0.27f, 0.27f, 0.27f);
     }
 
-   /* public void ItemSelected(int no)
-    {
-        if( no== 0)
-        {
-            ItemSelection[0].SetActive(true);
-            ItemSelection[1].SetActive(false);
-            ItemSelection[2].SetActive(false);
-        }
-        else if (no == 1)
-        {
-            ItemSelection[0].SetActive(false);
-            ItemSelection[1].SetActive(true);
-            ItemSelection[2].SetActive(false);
-        }
-        else if (no == 2)
-        {
-            ItemSelection[0].SetActive(false);
-            ItemSelection[1].SetActive(false);
-            ItemSelection[2].SetActive(true);
-        }
-    }
+    /* public void ItemSelected(int no)
+     {
+         if( no== 0)
+         {
+             ItemSelection[0].SetActive(true);
+             ItemSelection[1].SetActive(false);
+             ItemSelection[2].SetActive(false);
+         }
+         else if (no == 1)
+         {
+             ItemSelection[0].SetActive(false);
+             ItemSelection[1].SetActive(true);
+             ItemSelection[2].SetActive(false);
+         }
+         else if (no == 2)
+         {
+             ItemSelection[0].SetActive(false);
+             ItemSelection[1].SetActive(false);
+             ItemSelection[2].SetActive(true);
+         }
+     }
 
-    public void InfoButtonPressed(int no)
-    {
-        if(no == 0)
-        {
-            ResetCall();
-            InfoButtons[0].SetActive(false);
-            InfoPanels[0].SetActive(true);
-        }
-        else if(no == 1)
-        {
-            ResetCall();
-            InfoButtons[1].SetActive(false);
-            InfoPanels[1].SetActive(true);
-        }
-        else if(no == 2)
-        {
-            ResetCall();
-            InfoButtons[2].SetActive(false);
-            InfoPanels[2].SetActive(true);
-        }
-    }
+     public void InfoButtonPressed(int no)
+     {
+         if(no == 0)
+         {
+             ResetCall();
+             InfoButtons[0].SetActive(false);
+             InfoPanels[0].SetActive(true);
+         }
+         else if(no == 1)
+         {
+             ResetCall();
+             InfoButtons[1].SetActive(false);
+             InfoPanels[1].SetActive(true);
+         }
+         else if(no == 2)
+         {
+             ResetCall();
+             InfoButtons[2].SetActive(false);
+             InfoPanels[2].SetActive(true);
+         }
+     }
 
-    public void ResetCall()
-    {
-        for (int i = 0; i < InfoButtons.Length; i++)
-        {
-            InfoButtons[i].SetActive(true);
-            InfoPanels[i].SetActive(false);
-        }
-    }
+     public void ResetCall()
+     {
+         for (int i = 0; i < InfoButtons.Length; i++)
+         {
+             InfoButtons[i].SetActive(true);
+             InfoPanels[i].SetActive(false);
+         }
+     }
+
+     public void OnClick()
+     {
+         Application.OpenURL("https://www.youtube.com/watch?v=6WzU6jmXNIY");
+     }
+
+     public void ResetPosition()
+     {
+         for(int i = 0; i<ArtifactPos.Length;i++)
+         {
+             ArtifactPos[i].transform.rotation = Quaternion.Euler(0,90,0);
+         }
+     }*/
+
 
     public void OnClick()
     {
         Application.OpenURL("https://www.youtube.com/watch?v=6WzU6jmXNIY");
     }
 
-    public void ResetPosition()
-    {
-        for(int i = 0; i<ArtifactPos.Length;i++)
-        {
-            ArtifactPos[i].transform.rotation = Quaternion.Euler(0,90,0);
-        }
-    }*/
-
-   
-        
- 
 
     public void RoomSelection(int num)
     {
